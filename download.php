446f13be1d6ac4790f88b39da528b169161fda27
root <root@clients.dev-zero.org>
2015-10-06 20:06:17 -0400

    Initial Push

---
+++ download.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+<title>Download Glype</title>
+<meta name="robots" content="noarchive" />
+
+	
+<link rel="stylesheet" type="text/css" href="/style.css" media="all" />
+<script src="/javascript.js"></script>
+</head>
+<body>
+
+<div id="navcontainer">
+	<ul id="navlist">
+		<li class="navitem"><a href="https://www.glype.com/">Home</a></li>
+		<li class="navitem"><a href="https://www.glype.com/download.php" id="d1" onclick="setTimeout('downloaded(\'d1\')',200);">Download</a></li>
+		<li class="navitem"><a href="https://www.glype.com/contact.php">Contact Us</a></li>
+		<li class="navitem"><a href="https://www.glype.com/purchase.php">Licenses</a></li>
+	</ul>
+	<div class="hgradient">
+		<a href="/"><img src="/images/logo-small.png" class="hlogo" alt="" width="215" height="92" border="0"></a>
+	</div>
+</div>            
+<div id="gly-content" class="gly-wrapper"><div id="gly-content-left" class="glype"><h2 class="gly-top">Download Glype</h2><form method="POST" action="https://www.glype.com/download.php" id="download"><br>Please carefully review the following Software License Agreement. By clicking the &quot;I Agree, Download&quot; button below, you are confirming that you have read and understood the Software License Agreement and agree to be bound by all of its terms.<br><br><iframe src="/license.php?f=1" width="600" height="300" frameborder="1" marginheight="2" marginwidth="2" scrolling="true" allowtransparency="false"></iframe><br><br><input type="submit" name="sla" value=" I Agree, Download "></form><br></div></div><div class="clear"></div>
+<div id="gly-footer" class="gly-wrapper glype">
+	<p class="float-left">Glype website and proxy script &copy; 2006-2015 <a href="/">Glype.com</a>. All Rights Reserved.</p>
+	<p class="float-right"><a href="/license.php">Software License Agreement</a></p>
+	<div class="clear"></div>
+</div>
+
+
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://tally.upsideout.com/piwik/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "21"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript>
+<img src="https://tally.upsideout.com/piwik/piwik.php?idsite=21&amp;rec=1" style="border:0" alt="" />
+</noscript>
+
+
+</body>
+</html>
